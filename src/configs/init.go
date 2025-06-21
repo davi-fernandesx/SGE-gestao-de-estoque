58a1814479cae8961cc3a426a471200597422914
davi <davifernadesr@gmail.com>
2025-06-20 23:21:07 -0300

    remover a struct InitConnection e simplificar a inicialização da conexão com o banco de dados

--- src/configs/init.go
+++ src/configs/init.go
@@ -1,28 +1,43 @@
 package configs
 
 import (
-	
+	"database/sql"
 	"fmt"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 //struct da conexao para iniciar o banco de dados
-type InitConnection struct {
-	Conn *Connection
-}
-
-func (init *InitConnection) Init() ( *Connection, error){
 
 
-	db, err:= conn()//função conn vindo do arquivo que carrega a inicialização do banco de dados
-	if err != nil {
-		return nil, fmt.Errorf("erro ao chamar funcao Conn: %w", err)
-	}
+func InitApplication() ( *sql.DB, error){
 
-	log.Println("INFO: função para iniciar o banco de dados")
+	log.Println("SGE-SISTEMA DE GESTAO DE ESTOQUE!")
+	log.Print("desenvolvedores: Davi Fernandes e Paloma Brito" )
+		//carregando os arquivos .env
+		err:= godotenv.Load(".env")
+		if err != nil {
 	
-	init.Conn = db //adicionando a conexão da struct "Connection" a struct InitConnection
+			log.Printf("erro ao carregar o arquivo .env: %v", err)
+		}else {
+				log.Println("----------------------------------------------------------------")
+				log.Println("Carregando informações do banco de dados:")
+				log.Println("ARQUIVOS .ENV CARREGADOS")
+		}
+		
+		db, err:= conn()//função conn vindo do arquivo que carrega a inicialização do banco de dados
+		if err != nil {
+			return nil, fmt.Errorf("erro ao chamar funcao Conn: %w", err)
+		}
 
+		
+		if err != nil {
+
+			log.Printf("erro ao iniciar conexão: %v", err)
+		}
+
+			
 	return db, nil
 
 }
