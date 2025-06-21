7ff6ead57b8f919324506fc357525fb254ba115a
davi <davifernadesr@gmail.com>
2025-06-20 23:19:08 -0300

    refatorar a função main para simplificar a inicialização da aplicação e remover código desnecessário

--- src/main.go
+++ src/main.go
@@ -6,88 +6,27 @@ import (
 	"runtime"
 
 	"github.com/DaviFernandes034/SGE--gestao-de-estoque/configs"
-	"github.com/DaviFernandes034/SGE--gestao-de-estoque/controller"
-	"github.com/DaviFernandes034/SGE--gestao-de-estoque/repository"
-	"github.com/DaviFernandes034/SGE--gestao-de-estoque/services"
-	"github.com/joho/godotenv"
+	"github.com/DaviFernandes034/SGE--gestao-de-estoque/routes"
 )
 
-func main(){
+	func main(){
 
-	log.Println("SGE-SISTEMA DE GESTAO DE ESTOQUE!")
-	log.Print("desenvolvedor: Davi fernandes e Paloma")
-	//carregando os arquivos .env
-	err:= godotenv.Load(".env.docker")
-	if err != nil {
+			
+		db, err:= configs.InitApplication()
+		if err != nil {
+			log.Fatal(err)
+		}
+			mux:= routes.SetupRouter(db)
 
-		log.Printf("erro ao carregar o arquivo .env: %v", err)
-	}else {
+			log.Println("INFO: porta usada: :8080")
+			log.Println("INFO: aplicação iniciada com sucesso!!")
 			log.Println("----------------------------------------------------------------")
-		    log.Println("Carregando informações do banco de dados:")
-			log.Println("ARQUIVOS .ENV CARREGADOS")
+			printMemoryUsage()
+			
+			log.Fatal(http.ListenAndServe(":8080", mux))
+
+	
 	}
-
-	
-
-	var InitConnection configs.InitConnection //iniciando a inicializaçao do banco de dados
-
-	db ,err := InitConnection.Init() //chamando a funcao para iniciar a conexão
-	if err != nil {
-
-		log.Printf("erro ao iniciar conexão: %v", err)
-	}else{
-
-		log.Println("INFO: CONEXÃO COM O BANCO DE DADOS, COMPLETA!")
-
-		log.Println("INFO: aplicação iniciada com sucesso!!")
-		
-		log.Println("----------------------------------------------------------------")
-
-		log.Println("informações da aplicação: ")
-		log.Println("INFO: repository conectado com o banco de dados")
-		categoriaRepo:= repository.NewCategoriaRepository(db)
-		StatusRepo:= repository.NewStatusRepository(db)
-		ProdutoRepo:= repository.NewProdutoRepository(db)
-
-		log.Println("INFO: services conectado com a camada de repository")
-		categoriaService:= services.NewCategoriaService(categoriaRepo)
-		StatusService:= services.NewStatusService(StatusRepo)
-		ProdutoService:= services.NewProdutoService(ProdutoRepo)
-
-		log.Println("INFO: controller conectado com a camada de serviços")
-		categoriaController:= controller.NewCategoriaController(categoriaService)
-		StatusController:= controller.NewStatusController(StatusService)
-		ProdutoController:= controller.NewProdutoController(ProdutoService)
-
-		log.Println("INFO: ROTAS HTTP DE CATEGORIA, STATUS E PRODUTOS CARREGADAS!")
-		http.HandleFunc("/getCategoria", categoriaController.Get())
-		http.HandleFunc("/getAllCategoria", categoriaController.GetAll())
-		http.HandleFunc("/PostCategoria", categoriaController.Post())
-
-
-		http.HandleFunc("/getStatus", StatusController.Get())
-		http.HandleFunc("/getAllStatus", StatusController.GetAll())
-		http.HandleFunc("/PostStatus", StatusController.Post())
-
-
-		http.HandleFunc("/getProduto", ProdutoController.Get())
-		http.HandleFunc("/getAllProduto", ProdutoController.GetAll())
-		http.HandleFunc("/postProduto", ProdutoController.Post())
-
-
-		log.Println("INFO: porta usada: :8080")
-		log.Println("----------------------------------------------------------------")
-		printMemoryUsage()
-		
-		http.ListenAndServe(":8080", nil)
-
-		
-
-	}
-	
-	
-}
-
 
 
 func printMemoryUsage() {
