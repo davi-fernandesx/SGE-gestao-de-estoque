5fe834d67e0891c940c5c8ad0933fa939602e981
davi <davifernadesr@gmail.com>
2025-06-22 16:57:30 -0300

    adicionar funções utilitárias para resposta JSON para erro e para sucesso

---
+++ src/utils/helper.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func ResponseJSON(w http.ResponseWriter, statusCode int, data any){
+
+	w.Header().Set("content-type", "applicatioin/json")
+	w.WriteHeader(statusCode)
+
+	if data != nil {
+
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Printf("ERRO: falha ao decodificar resposta Json: %v",err)
+
+			http.Error(w, "erro interno", http.StatusInternalServerError)
+		}
+
+
+	}
+}
+
+func ResponseJsonError(w http.ResponseWriter, statusCode int, message string) {
+
+	errorResponse:= map[string]string{"error": message}
+
+	ResponseJSON(w, statusCode, errorResponse)
+}
