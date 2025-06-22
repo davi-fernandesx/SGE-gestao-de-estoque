2111193fd114c034e6d59e0db095c1e2f38f990e
davi <davifernadesr@gmail.com>
2025-06-22 18:13:26 -0300

    adicionar comentários sobre funcionalidades essenciais para o sistema de estoque

--- main.go
+++ main.go
@@ -36,3 +36,16 @@ func printMemoryUsage() {
    
     log.Printf("INFO: Memória total do sistema usada: %.2f MB\n", float64(m.Sys)/1024/1024)
 }
+
+
+
+/*
+		4. Adicione Funcionalidades Essenciais (Além do Básico)
+Mesmo para um MVP (Produto Mínimo Viável), algumas funcionalidades são quase obrigatórias em sistemas de estoque para serem vendáveis:
+
+Autenticação e Autorização: Controle de acesso por usuário e papel (administrador, operador, etc.). Essencial para segurança e colaboração.
+Relatórios Básicos: Listas de produtos, movimentações, estoque atual, produtos com estoque baixo. Não precisam ser complexos, mas precisam existir.
+Busca e Filtros Eficientes: Facilitar a localização de produtos.
+Histórico de Movimentações: Registrar quem fez o quê, quando e onde.
+Contagem de Estoque: Ferramenta para facilitar a contagem física e ajuste do estoque
+*/
