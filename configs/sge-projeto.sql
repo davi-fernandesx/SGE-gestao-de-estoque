d4255609d1cf87d21254fc203fc39d6e0903f4e0
davi <davifernadesr@gmail.com>
2025-07-05 21:32:22 -0300

    remover arquivos de configuração desnecessários e adicionar script SQL para criação de banco de dados e tabelas

---
+++ configs/sge-projeto.sql
@@ -0,0 +1,76 @@
+create database SGE;
+go
+use SGE;
+go
+
+CREATE TABLE Categorias(
+
+    ID_Categoria int PRIMARY KEY IDENTITY(1,1),
+    nome VARCHAR(20) not NULL unique,
+    criacao datetime,
+    atualizacao datetime
+);
+ go
+
+create table Produtos(
+
+    ID_Produto uniqueidentifier PRIMARY key default newid(),
+    nome VARCHAR(50) not null,
+    descricao text not null,
+    preco DECIMAL,
+    lote VARCHAR(20) not null,
+    quantidade int not null,
+    validade DATE not NULL,
+    ID_Categoria int not null,
+	ID_Status int not null,
+    criacao datetime ,
+    atualizacao datetime
+
+);
+go
+
+CREATE table status(
+    ID_Status int PRIMARY key IDENTITY(1,1),
+    nome VARCHAR(20) not null unique,
+    criacao datetime ,
+    atualizacao datetime
+);
+
+go
+alter table Produtos
+add CONSTRAINT fk_categoria
+FOREIGN key (ID_Categoria)
+REFERENCES Categorias(ID_Categoria);
+go
+alter table Produtos
+add constraint fk_status
+foreign key (ID_Status)
+references status(ID_Status);
+
+
+/* atualizando tabelas de produtos, categorias e status*/
+
+
+/* parte do usuario*/
+
+
+create table usuario (
+
+    ID int primary key  identity(1,1),
+    nome varchar(50) not null, 
+    email varchar(100) unique not null,
+    senha text not null,
+    criacao datetime,
+    atualizao datetime, 
+);
+
+go
+create table papeis (
+
+    id int primary key identity(1,1),
+    nome varchar(50) not null,
+    descricao text not null,
+
+);
+
+go
