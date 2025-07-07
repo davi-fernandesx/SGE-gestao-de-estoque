5dd7e8b95f94bae16b2b0d9ae87ad159f8bff37c
davi <davifernadesr@gmail.com>
2025-07-06 23:25:13 -0300

    implementar método Create no ProdutoRepository para inserir novos produtos no banco de dados

--- repository/produtosRepository.go
+++ repository/produtosRepository.go
@@ -23,7 +23,42 @@ func NewProdutoRepository(db *sql.DB) *ProdutoRepository {
 
 // Create implements interfaces.RepositoryCrud.
 func (p *ProdutoRepository) Create(entity models.Produtos) error {
-	return fmt.Errorf("teste")
+	
+	query:= `
+
+		insert into Produtos (nome, descricao, preco, lote, quantidade, validade, ID_Categoria, ID_Status, criacao, atualizacao, dataChegada)
+		values ( @nome, @descricao,@preco, @lote,@quantidade, @validade, @id_categoria, @id_status, @criacao, @atualizacao, @dataChegada);
+		 SELECT SCOPE_IDENTITY(); 
+	`
+
+	stmt, err:= p.Db.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(
+		sql.Named("nome", entity.Nome),
+		sql.Named("descricao", entity.Descricao),
+		sql.Named("preco", entity.Preco),
+		sql.Named("lote", entity.Lote),
+		sql.Named("quantidade", entity.Quantidade),
+		sql.Named("validade", entity.Validade),
+		sql.Named("id_categoria", entity.CategoriaId),
+		sql.Named("id_status", entity.StatusId),
+		sql.Named("criacao", entity.Criacao),
+		sql.Named("atualizacao", entity.Atualizacao),
+		sql.Named("dataChegada", entity.DataChegada),
+
+	)
+
+	defer stmt.Close()
+
+	if err != nil{
+		return  err
+	}
+
+	return  nil
+
 }
 
 // Delete implements interfaces.RepositoryCrud.
